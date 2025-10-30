7484d1a30590e0e9b219f7e672a4ed31791b729a
Akashgattu <160644848+Akashgattu@users.noreply.github.com>
2025-10-30 17:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+let prices=document.querySelectorAll(".price");
+let Total=0;
+prices.forEach((i)=>{
+	Total+=parseFloat(i.textContent);
+})
+let table=document.querySelector("table");
+let newrow=document.createElement("tr");
+let labelcell=document.createElement("td");
+let totalcell=document.createElement("td");
+labelcell.textContent= "Total Price: ";
+totalcell.textContent=Total;
+newrow.id='ans';
+newrow.append(labelcell,totalcell);
+table.appendChild(newrow);
+
   
 };
 
